9c7136eb0d4974a81418df31a2d67af92e16cc3f
Harshitha-B01 <harshitha995b@gmail.com>
2026-08-05 23:15:24 +0530

    Create install-tomcat.sh
    This script installs Tomcat 10, sets up Java, and deploys an application WAR file.

---
+++ install-tomcat.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+yum update -y
+
+yum install java-21-openjdk java-21-openjdk-devel -y
+
+java -version
+
+sudo dnf install -y wget
+
+cd /opt
+
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.26/bin/apache-tomcat-10.1.26.tar.gz
+
+if [ -f apache-tomcat-10.1.26.tar.gz ]; then
+    tar -xzf apache-tomcat-10.1.26.tar.gz
+else
+    echo "Tomcat download failed"
+    exit 1
+fi
+mv apache-tomcat-10.1.26 tomcat
+
+chmod +x /opt/tomcat/bin/*.sh
+
+/opt/tomcat/bin/startup.sh
+
+firewall-cmd --permanent --add-port=8080/tcp
+firewall-cmd --reload
+
+sleep 10
+ps -ef | grep tomcat
+
+/opt/tomcat/webapps/
+
+scp app.war ec2-user@<EC2-IP>:/opt/tomcat/webapps/
